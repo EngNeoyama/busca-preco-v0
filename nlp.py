a378b2511f8fef287efb28c0b793b819577db2f9
EngNeoyama <eng.neoyama@gmail.com>
2025-07-09 14:03:31 -0300

    nlp.py

--- nlp.py
+++ nlp.py
@@ -6,5 +6,8 @@ def extrair_parametros(texto):
     if match:
         preco_max = int(match.group(1))
 
-    termo = re.sub(r"até\s*R\$\s*\d+", "", texto).strip()
-    return {"termo": termo, "preco_max": preco_max}
+    # Remoção de vírgulas e termos desnecessários
+    termo_limpo = re.sub(r"[,:]", "", texto)
+    termo_limpo = re.sub(r"(?i)(marca|pot[êe]ncia|alimenta[çc][ãa]o|até R\$ \d+)", "", termo_limpo).strip()
+
+    return {"termo": termo_limpo, "preco_max": preco_max}
